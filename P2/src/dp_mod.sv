8b9d91e408c4c921d181c0b3674ac3ce3bcce14e
Juan Navarro <jnavsev@upv.edu.es>
2026-03-04 23:54:17 +0100

    Modificaciones tb y FM

--- P2/src/dp_mod.sv
+++ P2/src/dp_mod.sv
@@ -15,21 +15,45 @@ module dp_mod
 /* DECLARACIONES ------------------------- */
 
 // b0: ruta datos FM
-
+logic signed [16:0] b0_multiplicand_mux_r; // S[17,16]
+logic [32:0] b0_mult_res_full_s; // S[33,31]
+logic signed [23:0] b0_mult_res_r; // S[24,24]
+logic signed [24:0] b0_sum_res_extended_s; // S[25,24]
+logic signed [23:0] b0_sum_res_r; // S[24,24]
 // b1: ruta de datos AM
 
 // b2: DDS
 
 // b3: etapa final
 
-// b4: Generación  de oc_val_data
+// b4: Generaciï¿½n  de oc_val_data
 
 
 
 /* DESCRIPCION ------------------------- */	
 
 // b0: ruta datos FM
+always_ff @(posedge clk) begin
+    if(ic_fm_am) begin
+    b0_multiplicand_mux_r <= $signed({1'b0, id_im_fm});
+    end 
+        else begin
+            b0_multiplicand_mux_r <= 17'd0;
+        end
+end
 
+always_ff @(posedge clk) begin
+    if(ic_rst) begin
+        b0_mult_res_r <= 0;
+        b0_sum_res_r <= 0;
+        end 
+        else begin
+            b0_mult_res_full_s = id_data * b0_multiplicand_mux_r;
+            b0_mult_res_r <= b0_mult_res_full_s[30:30-23];
+            b0_sum_res_extended_s = {b0_mult_res_r[23], b0_mult_res_r} + $signed({1'b0, id_frec_por});
+            b0_sum_res_r <= b0_sum_res_extended_s[23:0];
+        end
+end
 // b1: ruta de datos AM
 
 // b2: DDS
